1b8cd0be1faf8e8d0b35fa00cc7c752a9a8c5092
zhuwc <zhuwc@jiedaibao.com>
2018-01-16 21:51:44 +0800

    init

---
+++ api/controllers/AController.php
@@ -0,0 +1,125 @@
+<?php
+namespace api\controllers;
+
+use Yii;
+use yii\data\ActiveDataProvider;
+use yii\web\NotFoundHttpException;
+use common\controllers\ActiveController;
+
+/**
+ * 基类控制器
+ *
+ * Class AController
+ * @package api\controllers
+ */
+class AController extends ActiveController
+{
+    public function actions()
+    {
+        $actions = parent::actions();
+        // 注销系统自带的实现方法
+        unset($actions['index'], $actions['update'], $actions['create'], $actions['delete'], $actions['view']);
+        // 自定义数据indexDataProvider覆盖IndexAction中的prepareDataProvider()方法
+        // $actions['index']['prepareDataProvider'] = [$this, 'indexDataProvider'];
+        return $actions;
+    }
+
+    /**
+     * 首页
+     *
+     * @return ActiveDataProvider
+     */
+    public function actionIndex()
+    {
+        $modelClass = $this->modelClass;
+        $query = $modelClass::find();
+
+        return new ActiveDataProvider([
+            'query' => $query,
+        ]);
+    }
+
+    /**
+     * 创建
+     *
+     * @return bool
+     */
+    public function actionCreate()
+    {
+        $model = new $this->modelClass();
+        $model->attributes = Yii::$app->request->post();
+        if (!$model->save())
+        {
+            // 返回数据验证失败
+            return $this->setResponse($this->analysisError($model->getFirstErrors()));
+        }
+
+        return $model;
+    }
+
+    /**
+     * 更新
+     *
+     * @param $id
+     * @return mixed
+     */
+    public function actionUpdate($id)
+    {
+        $model = $this->findModel($id);
+        $model->attributes = Yii::$app->request->post();
+        if (!$model->save())
+        {
+            // 返回数据验证失败
+            return $this->setResponse($this->analysisError($model->getFirstErrors()));
+        }
+
+        return $model;
+    }
+
+    /**
+     * 删除
+     *
+     * @param $id
+     * @return mixed
+     */
+    public function actionDelete($id)
+    {
+        return $this->findModel($id)->delete();
+    }
+
+    /**
+     * 显示单个
+     *
+     * @param $id
+     * @return mixed
+     */
+    public function actionView($id)
+    {
+        return $this->findModel($id);
+    }
+
+    /**
+     * 返回模型
+     *
+     * @param $id
+     * @return mixed
+     * @throws NotFoundHttpException
+     */
+    protected function findModel($id)
+    {
+        if (empty($id))
+        {
+            throw new NotFoundHttpException('请求的页面不存在.');
+        }
+
+        $modelClass = $this->modelClass;
+        if (empty(($model = $modelClass::findOne($id))))
+        {
+            return $model->loadDefaultValues();
+        }
+        else
+        {
+            throw new NotFoundHttpException('请求的页面不存在.');
+        }
+    }
+}
---
+++ frontend/widgets/MenuWidget.php
@@ -0,0 +1,42 @@
+<?php
+namespace frontend\widgets;
+
+use yii;
+use yii\base\Widget;
+use jianyan\basics\common\models\sys\DeskMenu;
+
+/**
+ * 前台导航widget
+ * Class MenuWidget
+ * @package frontend\widgets
+ */
+class MenuWidget extends Widget
+{
+    public function run()
+    {
+        $models = DeskMenu::find()
+            ->where(['status' => DeskMenu::STATUS_ON])
+            ->orderBy('sort ASC')
+            ->asArray()
+            ->all();
+
+        //控制器
+        $controller = Yii::$app->controller->id;
+
+        //判断当前控制器是在那个导航栏上面
+        foreach ($models as &$value)
+        {
+            $action = explode("/",$value['url']);
+
+            $value['present_action'] = false;
+            if ($action[1] == $controller)
+            {
+                $value['present_action'] = true;
+            }
+        }
+
+        return $this->render('menu/index', [
+            'models'=>$models,
+        ]);
+    }
+}
